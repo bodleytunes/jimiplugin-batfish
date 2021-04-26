d5306448ef0ab88d3a522dbfe59124bcf9d88214
root <jon.clayton@unipart.com>
2021-04-26 10:31:38 +0000

    few tweaks2

--- includes/batfish_route.py
+++ includes/batfish_route.py
@@ -6,14 +6,34 @@ from pybatfish.datamodel.flow import HeaderConstraints
 
 class batfish_route:
     def __init__(self, src_ip: str, dst_ip: str, dst_port: int) -> None:
+
         self.src_ip = src_ip
         self.dst_ip = dst_ip
         self.dst_port = dst_port
 
-    def validate_net(net: any) -> str:
+    def get_data(self):
+
+        batfish_routes = self.return_routes()
+
+        return batfish_routes
+
+    def return_routes(self) -> dict:
+
+        batfish_routes = {}
+
+        # do batfish
+        # TODO
+        # return routing results
+
+        return batfish_routes
+
+    def validate_net(self, net) -> str:
+        # Validate networks/ip's
+        # TODO
 
         return net
 
-    def validate_port(port: any) -> int:
-
+    def validate_port(self, port) -> int:
+        # Validate ports
+        # TODO
         return port
--- models/action.py
+++ models/action.py
@@ -20,13 +20,10 @@ class _batfish_route(action._action):
         dst_port = helpers.evalString(self.dst_port, {"data": data["flowData"]})
 
         # Call Batfish Function
-        batfish_route_checker = batfish_route.batfish_route(src_ip, dst_ip, dst_port)
-
-        route_list = None
-        route_list_answer = None
+        batfish_routes = batfish_route.batfish_route(src_ip, dst_ip, dst_port)
 
         # Return results
-        if len(route_list) > 0:
-            return {"result": True, "rc": 0, "data": route_list}
+        if len(batfish_routes) > 0:
+            return {"result": True, "rc": 0, "data": batfish_routes}
         else:
             return {"result": False, "rc": 404}
