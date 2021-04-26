78a7dd8541712927f6e624082c4305143ade868e
root <jon.clayton@unipart.com>
2021-04-26 09:14:17 +0000

    model registration

---
+++ batfish.py
@@ -0,0 +1,15 @@
+from core import plugin, model
+
+
+class _batfish(plugin._plugin):
+    version = 1.0
+
+    def install(self):
+        # Register batfish Models
+        model.registerModel("batfish","_batfish", "_action", "plugins.snmp.models.action")
+        return True
+
+    def uninstall(self):
+        # de-register batfish Models
+        model.deregisterModel("batfish","_batfish", "_action", "plugins.snmp.models.action")
+        return True
