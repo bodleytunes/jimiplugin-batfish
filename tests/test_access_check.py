9cffb0b30d9087c283a8414036c4b4f5cd5441ce
Jon Clayton <m87tech@gmail.com>
2021-05-10 10:35:44 +0100

    all results for all nodes

--- tests/test_access_check.py
+++ tests/test_access_check.py
@@ -19,7 +19,8 @@ src_ip = "10.1.255.100"
 destination_ip = "10.3.255.100"
 #applications =["dns"]
 applications =["icmp"]
-nodes = "spoke2"
+nodes = "spoke1"
+node_list = ["spoke1", "spoke2"]
 
 
 @pytest.mark.batfish
@@ -29,8 +30,9 @@ def main():
 
     
     results = ac.check(src_ip=src_ip, destination_ip=destination_ip, applications=applications, nodes=nodes, snapshot_folder="/shared/data/storage/firewall-configs/snapshot")
-
-
+    all_results = []
+    for node in node_list:
+        all_results.append(ac.check(src_ip=src_ip, destination_ip=destination_ip, applications=applications, nodes=node, snapshot_folder="/shared/data/storage/firewall-configs/snapshot"))
 
     pd.set_option("max_rows", None)
     pd.set_option("max_columns", None)
@@ -38,6 +40,7 @@ def main():
     #show_all(results)
     show_permitted(results)
     #show_denied(results)
+    show_permitted_all(all_results)
 
 
 
@@ -87,6 +90,34 @@ def show_permitted(results):
                                     if enum == 5:
                                         print(f"TraceTreeList: {e}")
                                         print("========================")
+
+def show_permitted_all(all_results):
+
+    # print only if accepted/permitted
+    for result in all_results:
+        for v in result.values:
+            if re.search("permit", v[3], re.IGNORECASE):
+                if len(v[5]) > 0:
+                    for item in v[5]:
+                        for item_child in item.children:
+                            for c in item_child.children:
+                                if re.search('permitted', c.traceElement.fragments[0].text, re.IGNORECASE):
+                                    for enum, e in enumerate(v):
+                                        if enum == 3:
+                                            print("========================")
+                                            print(f"Flow result : *** {e} ***")
+                                            continue
+                                        if enum == 0:
+                                            print(f"Node Queried is: {e}")
+                                        if enum == 1:
+                                            print(f"From zone/iface to zone/iface: {e}")
+                                        if enum == 2:
+                                            print(f"Flow details: {e}")
+                                        if enum == 4:
+                                            pass
+                                        if enum == 5:
+                                            print(f"TraceTreeList: {e}")
+                                            print("========================")
 
 def show_denied(results):
 
