f8828ddb5a6d160628c6eab1319bacfb978bceb9
Jon Clayton <m87tech@gmail.com>
2021-05-15 12:43:41 +0100

    WIP: testing simplyfying the returned results and putting into custom class/struct

--- tests/test_reachability.py
+++ tests/test_reachability.py
@@ -22,6 +22,7 @@ class ReachabilityResult:
         self,
     ):
         self.flow_result: FlowResult = None
+        self.trace_result: list[TraceResult] = []
 
         pass
 
@@ -40,6 +41,71 @@ class FlowResult(ReachabilityResult):
 
 
 # class TraceResult(ReachabilityResult):
+class TraceResult(ReachabilityResult):
+    def __init__(
+        self,
+    ):
+
+        self.trace_disposition: str
+        self.hops: list[Hop] = []
+
+
+class Hop(TraceResult):
+    def __init__(
+        self,
+    ):
+
+        self.node: str
+        self.steps: list[Step] = []
+
+
+class Step(Hop):
+    def __init__(
+        self,
+    ):
+
+        self.action: str
+        self.details: list[Detail] = []
+
+
+class Detail(Step):
+    def __init__(
+        self,
+    ):
+
+        self.originating_vrf: str
+        self.arp_ip: str
+        self.output_interface: str
+        self.routes: list[Route] = []
+        self.filter: str
+        self.filter_type: str
+        self.resolve_next_hop: str
+        self.flow = Flow()
+
+
+class Route(Detail):
+    def __init__(
+        self,
+    ):
+        self.network: str
+        self.next_Hop: str
+        self.via_protocol: str
+        pass
+
+
+class Flow(Detail):
+    def __init__(
+        self,
+    ):
+        self.src_ip: str
+        self.dst_ip: str
+        self.dst_port: str
+        self.ingress_node: str
+        self.ingress_interface: str
+        self.ingress_vrf: str
+        self.ip_protocol: str
+
+        pass
 
 
 start_location = "spoke1"
@@ -69,41 +135,61 @@ def main():
     flow = result.iloc[0]["Flow"]
     traces = result.iloc[0]["Traces"]
 
-    t_result = TraceResult(traces=traces, flow=flow)
+    rr = ReachabilityResult()
 
-    traces = t_result.traces
-
-    walk_flow(flow)
-    walk_traces(traces)
+    walk_flow(flow, rr)
+    walk_traces(traces, rr)
 
 
-def walk_flow(flow):
+def walk_flow(flow, rr):
     print(f"Flow Source IP: {flow.srcIp}")
     print(f"Flow Dest IP: {flow.dstIp}")
     print(f"Flow IP protocol: {flow.ipProtocol}")
     print(f"Flow Ingress Node: {flow.ingressNode}")
     print(f"Flow Ingress VRF: {flow.ingressVrf}")
 
-    rr = ReachabilityResult()
-
     rr.flow_result = FlowResult()
+
     rr.flow_result.src_ip = flow.srcIp
+    rr.flow_result.dst_ip = flow.dstIp
+    rr.flow_result.ip_protocol = flow.ipProtocol
+    rr.flow_result.ingress_node = flow.ingressNode
+    rr.flow_result.ingress_vrf = flow.ingressVrf
 
     print("hello")
 
 
-def walk_traces(traces):
+def walk_traces(traces, rr):
+
+    tr = TraceResult()
 
     for trace in traces:
         print(f"Trace dispostion: {trace.disposition}")
+
+        tr.trace_disposition = trace.disposition
+
         for hop in trace:
             print(f"hop node: {hop.node}")
-            for step in hop:
+
+            tr.hops = [Hop()]
+
+            for enum, step in enumerate(hop):
+
+                new_step = Step()
+                new_detail = Detail()
+                new_route = Route()
+
                 if step.action == "ORIGINATED":
                     print(f"step detail originating VRF: {step.detail.originatingVrf}")
+                    # add to detail
+                    new_detail.originating_vrf = step.detail.originatingVrf
+
                 elif step.action == "FORWARDED":
                     print(f"Arp IP: {step.detail.arpIp}")
+                    new_detail.arp_ip = step.detail.arpIp
                     print(f"Output Interface: {step.detail.outputInterface}")
+                    new_detail.output_interface = step.detail.outputInterface
+
                     for route in step.detail.routes:
                         # Put assertions in here?
                         print(f"Network: {route['network']}")
@@ -124,6 +210,13 @@ def walk_traces(traces):
                 elif step.action == "EXITS_NETWORK":
                     print(f"Output Interface: {step.detail.outputInterface}")
                     print(f"Next-hop IP: {step.detail.resolvedNexthopIp}")
+
+                # append stuff
+                new_step.details.append(new_detail)
+                tr.hops[enum].steps.append(new_step)
+
+        # add to the list of trace results
+        rr.trace_result.append(tr)
 
 
 class TraceResult:
