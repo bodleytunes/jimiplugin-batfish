6142b7f4f5fa8dded1565c60c0fe16331467cc9b
Jon Clayton <m87tech@gmail.com>
2021-05-20 10:54:04 +0100

    moving files around

---
+++ includes/queries/access_check.py
@@ -0,0 +1,285 @@
+from typing import Any, Dict, Optional, Union, List
+import re
+from collections import defaultdict
+
+from plugins.batfish.includes.batfish import Batfish
+from plugins.batfish.includes.result_models.access import AcceptResult, DeniedResult
+
+
+class AccessCheck(Batfish):
+    def __init__(
+        self,
+        batfish_server: Optional[str] = None,
+        host: Optional[str] = None,
+        ingress: Optional[str] = None,
+        src_ip: Optional[str] = None,
+        destination_ip: Optional[str] = None,
+        applications: Optional[str] = None,
+        dst_ports: Optional[str] = None,
+        ip_protocols: Optional[list] = None,
+        nodes: Optional[str] = None,
+        snapshot_folder: Optional[str] = None,
+        b_fish=None,
+    ):
+
+        self.batfish_server = batfish_server
+        self.host = host
+        self.src_ip = src_ip
+        self.destination_ip = destination_ip
+        self.applications = applications
+        self.dst_ports = dst_ports
+        self.ip_protocols = ip_protocols
+        self.snapshot_folder = snapshot_folder
+        self.nodes = "hub2"
+
+        # Instance of batfish object
+        self.b_fish = b_fish
+
+        pass
+
+    """
+    Name: Get Results
+    Overview: 
+    Args: Params relating to checking access/policy
+    Returns: Processed Access results
+    """
+
+    def get_results(
+        self,
+        src_ip=None,
+        destination_ip=None,
+        applications=None,
+        dst_ports=None,
+        ip_protocols=None,
+        nodes=None,
+    ) -> Dict[str, Any]:
+
+        self.src_ip = src_ip
+        self.destination_ip = destination_ip
+        self.applications = applications
+        self.dst_ports = dst_ports
+        self.ip_protocols = ip_protocols
+        self.nodes = nodes
+
+        results_dict = defaultdict(list)
+
+        #! todo this needs refactoring
+        for node in self.nodes:
+
+            result = self.check()
+
+            results_dict[node].append(result)
+
+        (
+            deny_results,
+            accept_results,
+        ) = self._build_results(results_dict)
+
+        return deny_results, accept_results
+
+    """
+    Name: Make Query
+    Args: Params relating to checking access/policy
+    Returns: Batfish Query Dataframe
+    """
+
+    def check(
+        self,
+    ):
+
+        if self.applications is not None:
+
+            flow = self.b_fish.hc(
+                srcIps=self.src_ip,
+                dstIps=self.destination_ip,
+                applications=self._filter_text(self.applications),
+            )
+
+        elif self.dst_ports is not None:
+
+            flow = self.b_fish.hc(
+                srcIps=self.src_ip,
+                dstIps=self.destination_ip,
+                dstPorts=self._filter_text(self.dst_ports),
+                ipProtocols=self._make_upper(self.ip_protocols),
+            )
+
+        query = self.b_fish.bfq.testFilters(headers=flow, nodes=self.nodes)
+
+        result = query.answer().frame()
+
+        return result
+
+    def _build_results(self, results_dict) -> Union[AcceptResult, DeniedResult]:
+
+        accept_results = []
+        denied_results = []
+
+        for node, result in results_dict.items():
+
+            for r in result:
+
+                for v in r.values:
+                    if re.search("permit", v[3], re.IGNORECASE):
+                        accept_result = AcceptResult()
+                        accept_result.query_node = node
+
+                        if len(v[5]) > 0:
+                            for item in v[5]:
+                                for item_child in item.children:
+                                    for c in item_child.children:
+                                        if re.search(
+                                            "permitted",
+                                            c.traceElement.fragments[0].text,
+                                            re.IGNORECASE,
+                                        ):
+                                            for enum, e in enumerate(v):
+                                                if enum == 3:
+                                                    accept_result.flow_result = e
+                                                    continue
+                                                if enum == 0:
+                                                    print(f"Node Queried is: {e}")
+
+                                                if enum == 1:
+
+                                                    accept_result.ingress_egress = e
+
+                                                    # split ingress egress string
+                                                    (
+                                                        ingress_zone,
+                                                        ingress_iface,
+                                                        egress_zone,
+                                                        egress_iface,
+                                                    ) = self._split_ingress_egress(
+                                                        accept_result.ingress_egress
+                                                    )
+                                                    accept_result.ingress_zone = (
+                                                        ingress_zone
+                                                    )
+                                                    accept_result.ingress_interface = (
+                                                        ingress_iface
+                                                    )
+                                                    accept_result.egress_zone = (
+                                                        egress_zone
+                                                    )
+                                                    accept_result.egress_interface = (
+                                                        egress_iface
+                                                    )
+                                                if enum == 2:
+                                                    accept_result.flow_details = e
+                                                    # other details
+                                                    accept_result.destination_address = (
+                                                        accept_result.flow_details.dstIp
+                                                    )
+                                                    accept_result.source_address = (
+                                                        accept_result.flow_details.srcIp
+                                                    )
+                                                    accept_result.service = (
+                                                        accept_result.flow_details.ipProtocol
+                                                    )
+                                                    accept_result.ingress_node = (
+                                                        accept_result.flow_details.ingressNode
+                                                    )
+                                                    accept_result.ingress_vrf = (
+                                                        accept_result.flow_details.ingressVrf
+                                                    )
+                                                    accept_result.flow_details = None
+
+                                                if enum == 4:
+                                                    pass
+                                                if enum == 5:
+                                                    accept_result.trace_tree_list = e
+                                                    # get policy permit details / rule details
+                                                    accept_result.permit_rule = (
+                                                        accept_result.trace_tree_list[0]
+                                                        .traceElement.fragments[1]
+                                                        .text
+                                                    )
+                                                    accept_result.rule_id = (
+                                                        accept_result.trace_tree_list[0]
+                                                        .traceElement.fragments[2]
+                                                        .text
+                                                    )
+                                                    accept_result.trace_tree_list = None
+
+                        if accept_result.flow_result == "PERMIT":
+                            accept_results.append(accept_result)
+
+                    else:
+                        # generate a DENY entry
+                        denied_result = DeniedResult()
+
+                        denied_result.denied == True
+                        denied_result.query_node = node
+
+                        denied_results.append(denied_result)
+
+        # then whittle all denied results down to only one per query node
+        # merged_results = [*accept_results, *denied_results]
+        # if entry in access results then remove all from denied
+        traffic_denied_hosts = self._remove_denied(accept_results, denied_results)
+        traffic_denied_hosts = self._remove_dupes(traffic_denied_hosts)
+
+        # empty denied results list
+        denied_results.clear()
+        # create a new unique list of denied results
+        for i in traffic_denied_hosts:
+            for item in list(i):
+                denied_result = DeniedResult()
+                denied_result.query_node = item
+                denied_result.denied == True
+                denied_results.append(denied_result)
+
+        # convert objects to dictionaries
+
+        deny_results = [obj.__dict__ for obj in denied_results]
+        accept_results = [accept_result.__dict__ for accept_result in accept_results]
+
+        return deny_results, accept_results
+
+    def _split_ingress_egress(self, ingress_egress):
+
+        split_list = ingress_egress.split("~")
+
+        ingress_zone = split_list[0]
+        ingress_iface = split_list[1]
+        egress_zone = split_list[3]
+        egress_iface = split_list[4]
+
+        return ingress_zone, ingress_iface, egress_zone, egress_iface
+
+    def _remove_denied(self, accept_results, denied_results) -> List[DeniedResult]:
+        # remove denied entries
+        for a in accept_results:
+            # iterate over copy as don't delete from list while iterating
+            for d in list(denied_results):
+                if d.query_node == a.query_node:
+                    # remove this denied entry
+                    denied_results.remove(d)
+
+        return denied_results
+
+    def _remove_dupes(self, denied_results) -> list:
+
+        # make unique/remove repeated denies/duplicate nodes
+        denied_hosts_list = [d.query_node for d in denied_results]
+        unique_denied_hosts_list = set(denied_hosts_list)
+        # to list
+        traffic_denied_hosts = [unique_denied_hosts_list]
+
+        return traffic_denied_hosts
+
+    def _filter_text(self, arg) -> str:
+        # filter empty "" or ''
+        if arg:
+            converter = lambda i: i or None
+            result = [converter(i) for i in arg]
+            return result
+        return
+
+    def _make_upper(self, arg) -> str:
+        #  make protocols uppercase
+        if arg:
+            result = [x.upper() for x in arg]
+            return result
+        return
---
+++ includes/result_models/access.py
@@ -0,0 +1,58 @@
+from typing import Any, Dict, Optional, Union, List
+
+
+class AcceptResult(object):
+    def __init__(
+        self,
+        query_node: Optional[str] = None,
+        flow_result: Optional[str] = None,
+        flow_details: Optional[str] = None,
+        trace_tree_list: Optional[str] = None,
+        ingress_egress: Optional[str] = None,
+        ingress_zone: Optional[str] = None,
+        ingress_interface: Optional[str] = None,
+        egress_interface: Optional[str] = None,
+        egress_zone: Optional[str] = None,
+        ingress_vrf: Optional[str] = None,
+        ingress_node: Optional[str] = None,
+        source_address: Optional[str] = None,
+        destination_address: Optional[str] = None,
+        service: Optional[str] = None,
+        ip_protocol: Optional[str] = None,
+        permit_rule: Optional[str] = None,
+        rule_id: Optional[str] = None,
+        result_data: Optional[str] = None,
+    ) -> None:
+        pass
+
+        self.query_node = query_node
+        self.flow_result = flow_result
+        self.flow_details = flow_details
+        self.trace_tree_list = trace_tree_list
+        self.ingress_egress = ingress_egress
+        self.ingress_zone = ingress_zone
+        self.ingress_interface = ingress_interface
+        self.egress_interface = egress_interface
+        self.egress_zone = egress_zone
+        self.ingress_vrf = ingress_vrf
+        self.ingress_node = ingress_node
+        self.source_address = source_address
+        self.destination_address = destination_address
+        self.service = service
+        self.ip_protocol = ip_protocol
+        self.permit_rule = permit_rule
+        self.rule_id = rule_id
+
+        self.result_data = result_data
+
+
+class DeniedResult(object):
+    def __init__(
+        self,
+        query_node: Optional[str] = None,
+        denied: Optional[str] = None,
+    ) -> None:
+        pass
+
+        self.query_node = query_node
+        self.denied = True
