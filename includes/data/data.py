1d112dabe7a06df9b24d243397b841673a4f87c6
Jon Clayton <m87tech@gmail.com>
2021-05-24 17:22:08 +0100

    builder class testing

--- includes/data/data.py
+++ includes/data/data.py
@@ -1,198 +1,65 @@
-from includes.data.builder import DataBuilder
-from includes import data
-from logging import exception
-from typing import Optional, List, Tuple, DefaultDict, Any
+from abc import ABC, abstractmethod
+import sys
+import os
+from typing import Any, DefaultDict, List, Tuple
 import re
-from collections import defaultdict
+from logging import exception
 
-import pandas as pd
+PACKAGE_PARENT = "../../../../"
+SCRIPT_DIR = os.path.dirname(
+    os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__)))
+)
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+# end fudge python path
 
-from pybatfish.exception import BatfishException
 
 from plugins.batfish.includes.batfish import Batfish
-from plugins.batfish.includes.result_models.access import AcceptResult, DeniedResult
 from plugins.batfish.includes.bat_helpers import BatHelpers
-from plugins.batfish.includes.data.builder import AccessDataBuilder
+from plugins.batfish.includes.result_models.access import AcceptResult, DeniedResult
+from plugins.batfish.includes.queries.access_check import AccessCheck
 
 
-class AccessCheck(Batfish):
-    def __init__(
+class Walkable(AccessCheck, ABC):
+    @abstractmethod
+    def walk_data(
         self,
-        batfish_server: Optional[str] = None,
-        host: Optional[str] = None,
-        src_ip: Optional[str] = None,
-        dst_ip: Optional[str] = None,
-        applications: Optional[list] = None,
-        dst_ports: Optional[str] = None,
-        ip_protocols: List[Any] = None,
-        nodes: Optional[str] = None,
-        snapshot_folder: Optional[str] = None,
-        b_fish=None,
     ):
-
-        self.batfish_server = batfish_server
-        self.host = host
-        self.src_ip = src_ip
-        self.dst_ip = dst_ip
-        self.applications = applications
-        self.dst_ports = dst_ports
-        self.ip_protocols = ip_protocols
-        self.snapshot_folder = snapshot_folder
-        # self.nodes = "hub2"
-
-        # Instance of a batfish object
-        self.b_fish = b_fish
-
         pass
 
-    def get_results(
-        self,
-        src_ip: Optional[str] = None,
-        dst_ip: Optional[str] = None,
-        applications: Optional[list] = None,
-        dst_ports: Optional[str] = None,
-        ip_protocols: List[Any] = None,
-        nodes: Optional[list] = None,
-    ) -> Tuple[List[dict], List[dict]]:
-        """Get Batfish Results
 
-        Args:
-            src_ip (string, optional): source ip. Defaults to None.
-            dst_ip (string, optional): destination ip. Defaults to None.
-            applications (list, optional): list of applications. Defaults to None.
-            dst_ports (str, optional): list of destination ports. Defaults to None.
-            ip_protocols (list, optional): list of protocols. Defaults to None.
-            nodes (list, optional): list of nodes(network devices/firewalls etc)to query. Defaults to None.
-
-        Returns:
-            Tuple[List[dict], List[dict]]: 2 variables, each a list of dictionaries for permitted and denied results
-        """
+class DataWalker(Walkable):
+    def __init__(self, src_ip, dst_ip, results_dict: DefaultDict[str, List[Any]]):
 
         self.src_ip = src_ip
         self.dst_ip = dst_ip
-        self.applications = applications
-        self.dst_ports = dst_ports
-        self.ip_protocols = ip_protocols
-        self.nodes = nodes
+        self.results_dict = results_dict
 
-        # make pre-check and validation on input data
-        self._pre_flight_checks()
+    @abstractmethod
+    def walk_data(self):
+        # create flow
+        pass
 
-        # create empty list for returned results (Accept and Deny results)
-        self.results_dict = defaultdict(list)
 
-        # Loop through all passed in nodes(Network devices/Firewalls)
-        for node in self.nodes:
+class AccessCheckDataWalker(DataWalker):
+    nodes: list
 
-            # Run access checker function to make the batfish query on a per node basis and return a result for each of them
-            self._query(node)
+    src_ip: str
+    dst_ip: str
+    dst_ports: str
 
-            # Append each nodes query result to the list
-            # results_dict[node].append(result)
+    def __init__(self):
+        self.port = None
+        self.flow = None
+        self.query = None
 
-        # process results (walk dataframe) and return dictionary type data suitable for merging with eventData
-        (
-            deny_results,
-            accept_results,
-        ) = self._build_results(self.results_dict)
-
-        # return both deny and results variables to "action"
-        return deny_results, accept_results
-
-    def _query(self, nodes: Optional[list] = None) -> pd.DataFrame:
-        """Do Batfish Query
-
-        Args:
-            nodes (list, optional): list of nodes to query. Defaults to None.
-
-        Raises:
-            BatfishException: batfish exception
-
-        Returns:
-            pd.DataFrame: returns nested structured data frame
-        """
-        if len(self.applications) > 0:
-
-            # flow is a headerConstraint object which was built from passing in args relating to the source/dst ip/proto (5 tuple etc)
-            flow = self.b_fish.hc(
-                srcIps=self.src_ip, dstIps=self.dst_ip, applications=self.applications
-            )
-        elif len(self.dst_ports) > 0 and len(self.ip_protocols) > 0:
-            # send dst_ports to splitter helper
-            self._split_ports(self.dst_ports)
-            # there are more than one port returned in the list then iterate through ports and make a query on each
-            if len(self.dst_ports_list) > 1:
-                print("multiple ports")
-                for port in self.dst_ports_list:
-                    flow = self.b_fish.hc(
-                        srcIps=self.src_ip,
-                        dstIps=self.dst_ip,
-                        dstPorts=port,
-                        ipProtocols=BatHelpers.make_upper(self.ip_protocols),
-                    )
-                    self._make_query(flow, nodes)
-
-            flow = self.b_fish.hc(
-                srcIps=self.src_ip,
-                dstIps=self.dst_ip,
-                dstPorts=self.dst_ports,
-                ipProtocols=BatHelpers.make_upper(self.ip_protocols),
-            )
-            self._make_query(flow, nodes)
-        elif len(self.dst_ports) > 0:
-            flow = self.b_fish.hc(
-                srcIps=self.src_ip,
-                dstIps=self.dst_ip,
-                dstPorts=self.dst_ports,
-            )
-            self._make_query(flow, nodes)
-
-        elif len(self.ip_protocols) > 0:
-            flow = self.b_fish.hc(
-                srcIps=self.src_ip,
-                dstIps=self.dst_ip,
-                ipProtocols=self.ip_protocols,
-            )
-            self._make_query(flow, nodes)
-
-    def _make_query(self, flow, nodes):
-        """
-        make query
-        """
-        # nodes is actually a single node here, not sure why batfish have named it "nodes"?
-        # flow is a headerConstraint object which was built from passing in args relating to the source/dst ip/proto (5 tuple etc)
-        try:
-            query = self.b_fish.bfq.testFilters(headers=flow, nodes=nodes)
-            result = query.answer().frame()
-            # Append each nodes query result to the results_dict list
-            self.results_dict[nodes].append(result)
-
-        except BatfishException as e:
-            print(e)
-            raise BatfishException(f"Batfish Query failure :  {e}")
-
-    def _build_results(
-        self, results_dict: DefaultDict[str, List[Any]]
-    ) -> Tuple[List[dict], List[dict]]:
-        """Build event data results
-
-        Args:
-            results_dict (List[pd.DataFrame]): Pass in list of dataframes (one per node)
-
-        Returns:
-            Tuple[List[dict], List[dict]]: 2 Lists of dicts for denied results and accept results
-        """
-        # test
-        data_builder = DataBuilder()
-        data_builder.build_data(results_dict)
-        return data_builder.deny_results, data_builder.accept_results
-        # end test
-
+    def walk_data(
+        self,
+    ):
         accept_results = list()
         denied_results = list()
 
         # Walks its way through a deply nested dataframe type structure
-        for node, result in results_dict.items():
+        for node, result in self.results_dict.items():
 
             for r in result:
 
@@ -294,35 +161,12 @@ class AccessCheck(Batfish):
 
                     else:
                         # generate a DENY entry
-                        denied_result = DeniedResult()
-
-                        denied_result.denied = True
-                        denied_result.query_node = node
+                        denied_result = DeniedResult(denied=True, query_node=node)
 
                         denied_results.append(denied_result)
 
-        # then whittle all denied results down to only one per query node
-        # if we find a node entry in accessresults then remove all from denied
-        traffic_denied_hosts = self._remove_denied(accept_results, denied_results)
-        traffic_denied_hosts = self._remove_dupes(traffic_denied_hosts)
+        # split data delimited by ~ into separate strings
 
-        # empty denied results list
-        denied_results.clear()
-        # create a new unique list of denied results
-        for i in traffic_denied_hosts:
-            for item in list(i):
-                denied_result = DeniedResult()
-                denied_result.query_node = item
-                denied_result.denied = True
-                denied_results.append(denied_result)
-
-        # convert the AcceptResult() and DenyResult() objects to dictionaries so they can be consumed by eventData
-        deny_results = [obj.__dict__ for obj in denied_results]
-        accept_results = [obj.__dict__ for obj in accept_results]
-
-        return deny_results, accept_results
-
-    # split data delimited by ~ into separate strings
     def _split_ingress_egress(self, ingress_egress: str) -> Tuple[str, str, str, str]:
 
         split_list = ingress_egress.split("~")
@@ -357,7 +201,25 @@ class AccessCheck(Batfish):
 
         return traffic_denied_hosts
 
-    def _pre_flight_checks(self):
+
+class PreFlight(ABC):
+    @abstractmethod
+    def run_pre_flight_checks(self):
+        pass
+
+
+class DataWalkerPreflight:
+    def __init__(self, p: PreFlight):
+        # shared vars go here
+        self.src_ip
+        self.dst_ip
+
+
+class AccessCheckPreFlight(PreFlight):
+    def __init__(self) -> None:
+        pass
+
+    def run_pre_flight_checks(self):
 
         # validate IP's
         if self.src_ip:
@@ -376,8 +238,3 @@ class AccessCheck(Batfish):
             self.src_ip = "0.0.0.0/0"
         if not self.dst_ip:
             self.dst_ip = "0.0.0.0/0"
-
-    def _split_ports(self, dst_ports):
-        # split out ports
-        # if contains comma
-        self.dst_ports_list = re.split(",|-", self.dst_ports)
