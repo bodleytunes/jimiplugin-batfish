14d7f42cd6dab1ab007f362d425052cecf51bfc6
Jon Clayton <supertent@gmail.com>
2021-06-07 11:46:44 +0100

    Feature/general fixes and mods (#25)
    * just a few tidy ups and a comment
    
    * changing the name of an attribute to better suit
    
    * changing the name of an attribute to better suit
    
    * tweaks
    
    * adding a check to see if the dst_folder key exists in eventdata, which if true, it proceeds to copy the files/dir to a new one under /tmp which has the snapshot/configs format
    
    Co-authored-by: Jon Clayton <m87tech@gmail.com>

--- includes/result_models/reachability.py
+++ includes/result_models/reachability.py
@@ -89,8 +89,6 @@ class Flow(Detail):
         self.src_ip: str
         self.dst_ip: str
         self.dst_port: str
-
-
         self.destination_ingress_node: str
         self.ingress_interface: str
         self.ingress_vrf: str
